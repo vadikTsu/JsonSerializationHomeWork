71c810338c636907d5d73c6dd9feae15152d2bb0
vadik <vadimtsudenko@gmail.com>
2023-10-21 08:18:12 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+
+    public class PublishingHouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+
+    public class Book
+    {
+        [JsonIgnore]
+        public int PublishingHouseId { get; set; }
+
+        [JsonProperty("Name")]
+        public string Title { get; set; }
+
+        public PublishingHouse PublishingHouse { get; set; }
+    }
+
+    public class BookService
+    {
+        public List<Book> LoadBooks(string filePath) 
+        {
+            try {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<Book>>(json); 
+            } 
+            catch
+            { 
+                Console.WriteLine("file not found " + filePath);
+                return null;
+            }
+           
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BookService bookService = new BookService();
+            List<Book> books = bookService.LoadBooks("path_to_file.json");
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
